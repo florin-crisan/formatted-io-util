8325eda2f566518eecdf3c665b78707bf82d1bd6
Florin Crișan <florin.crisan@gmail.com>
2021-04-12 19:29:57 +0300

    WIP: Beautify type names

--- src/type_info.cpp
+++ src/type_info.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 #include <boost/smart_ptr.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string_regex.hpp>
 #include <fcrisan/formatted-io-util/type_info.hpp>
 
 #ifdef __GNUC__
@@ -9,19 +13,111 @@
 
 namespace {
 
-void DoNothing(const void *) {}
-
-boost::shared_ptr<const char> Demangle(const char *mangled) {
+std::string Demangle(const char *mangled) {
 #ifdef __GNUC__
 	boost::shared_ptr<char> demangled(abi::__cxa_demangle(mangled, 0, 0, 0), &free);
 	if (demangled)
-		return demangled;
+		return demangled.get();
 #endif
-	return boost::shared_ptr<const char>(mangled, DoNothing);
+	return mangled;
+}
+
+inline std::string Demangle(const std::type_info &type) {
+	return Demangle(type.name());
 }
 
 } // anonymous namespace
 
 std::ostream & operator<<(std::ostream &stream, const std::type_info &type) {
-	return stream << Demangle(type.name());
+	using namespace std;
+	string typeName = Demangle(type);
+
+#ifdef FCRISAN_FORMATTED_IO_UTIL_BEAUTIFY_TYPENAMES
+	using namespace boost;
+	using namespace boost::algorithm;
+	using namespace boost::regex_constants;
+
+	struct Replacement {
+		string what;
+		const char *replaceWith;
+	};
+	// Order is important here: replace the full name of string before trying to get rid of 'class' and 'struct'.
+	static const Replacement replacements[] = {
+		// prefer short names for standard types, i.e. 'std::string' instead of 'class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >'
+#define SHORTNAME(type) {Demangle(typeid(type)), #type}
+
+		SHORTNAME(std::string),
+		SHORTNAME(std::wstring),
+
+		SHORTNAME(std::iostream),
+		SHORTNAME(std::wiostream),
+		SHORTNAME(std::istream),
+		SHORTNAME(std::wistream),
+		SHORTNAME(std::ostream),
+		SHORTNAME(std::wostream),
+
+		SHORTNAME(std::fstream),
+		SHORTNAME(std::wfstream),
+		SHORTNAME(std::ifstream),
+		SHORTNAME(std::wifstream),
+		SHORTNAME(std::ofstream),
+		SHORTNAME(std::wofstream),
+
+		SHORTNAME(std::stringstream),
+		SHORTNAME(std::wstringstream),
+		SHORTNAME(std::istringstream),
+		SHORTNAME(std::wistringstream),
+		SHORTNAME(std::ostringstream),
+		SHORTNAME(std::wostringstream),
+
+#ifdef _MSC_VER
+		SHORTNAME(long long), // instead of '__int64'
+		{"* __ptr64", "*"},
+#endif // _MSC_VER
+	};
+
+	for (size_t i = 0; i < sizeof(replacements)/sizeof(replacements[0]); ++i) {
+		const Replacement &r = replacements[i];
+		replace_all(typeName, r.what, r.replaceWith);
+	}
+
+	struct RegexReplacement {
+		regex what;
+		string replaceWith;
+	};
+	static const RegexReplacement regexReplacements[] = {
+		{
+			// trim whitespace from '*'
+			regex("\\s*\\*\\s*", format_perl),
+			"*"
+		},
+#ifdef _MSC_VER
+		{
+			// 'class std::exception' --> 'std::exception'
+			regex("\\<class\\>\\s*", format_perl),
+			""
+		},
+		{
+			regex("\\<struct\\>\\s*", format_perl),
+			""
+		},
+#endif // _MSC_VER
+		{
+			// std::map<std::string, std::string, std::less<std::string>, std::allocator<std::pair<std::string const, std::string> > >
+			regex("std::map\\s*<\\s*([a-zA-Z0-9_:]+)\\s*,\\s*([a-zA-Z0-9_:]+)\\s*,\\s*"
+				"std::less\\s*<\\s*\\1\\s*>\\s*,\\s*"
+				"std::allocator\\s*<\\s*std::pair\\s*<\\s*\\1\\s+const\\s*,\\s*\\2\\s*>\\s*>\\s*>\\s*",
+				format_perl),
+			"std::map<$1, $2>"
+		}
+	};
+
+	for (size_t i = 0; i < sizeof(regexReplacements) / sizeof(regexReplacements[0]); ++i) {
+		const RegexReplacement &r = regexReplacements[i];
+		replace_all_regex(typeName, r.what, r.replaceWith);
+	}
+
+#endif // FCRISAN_FORMATTED_IO_UTIL_BEAUTIFY_TYPENAMES
+
+	return stream << typeName;
 }
--- unittest/type_info.cpp
+++ unittest/type_info.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include <map>
 #include <boost/test/auto_unit_test.hpp>
 #include <fcrisan/formatted-io-util/type_info.hpp>
 
@@ -7,10 +9,45 @@ std::string Write(const std::type_info &value) {
 	return stream.str();
 }
 
+struct somestruct123 {};
+class someclassoranother {};
+class Struct {};
+struct Class {};
+
 BOOST_AUTO_TEST_CASE(WriteTypeId)
 {
 	BOOST_CHECK_EQUAL(Write(typeid(bool)), "bool");
 	BOOST_CHECK_EQUAL(Write(typeid(char)), "char");
+	BOOST_CHECK_EQUAL(Write(typeid(unsigned char)), "unsigned char");
+	BOOST_CHECK_EQUAL(Write(typeid(signed char)), "signed char");
+	BOOST_CHECK_EQUAL(Write(typeid(short)), "short");
+	BOOST_CHECK_EQUAL(Write(typeid(unsigned short)), "unsigned short");
 	BOOST_CHECK_EQUAL(Write(typeid(int)), "int");
-	BOOST_CHECK(Write(typeid(std::exception)).find("std::exception") != std::string::npos);
+	BOOST_CHECK_EQUAL(Write(typeid(unsigned)), "unsigned int");
+	BOOST_CHECK_EQUAL(Write(typeid(long)), "long");
+	BOOST_CHECK_EQUAL(Write(typeid(unsigned long)), "unsigned long");
+	BOOST_CHECK_EQUAL(Write(typeid(long long)), "long long");
+	BOOST_CHECK_EQUAL(Write(typeid(unsigned long long)), "unsigned long long");
+
+	// Should remove 'class' and 'struct' from types (they are added on Windows)
+	BOOST_CHECK_EQUAL(Write(typeid(std::exception)), "std::exception");
+	// but only if it's a lowercase word
+	BOOST_CHECK_EQUAL(Write(typeid(Struct)), "Struct");
+	BOOST_CHECK_EQUAL(Write(typeid(Class)), "Class");
+	BOOST_CHECK_EQUAL(Write(typeid(somestruct123)), "somestruct123");
+	BOOST_CHECK_EQUAL(Write(typeid(someclassoranother)), "someclassoranother");
+
+
+	BOOST_CHECK_EQUAL(Write(typeid(void *)), "void*");
+	BOOST_CHECK_EQUAL(Write(typeid(char *)), "char*");
+	// TODO: Regularize const position, i.e., 'const char *' instead of the more confusing 'char const *'
+	// BOOST_CHECK_EQUAL(Write(typeid(const char *)), "const char*");
+
+	BOOST_CHECK_EQUAL(Write(typeid(std::string)), "std::string");
+	BOOST_CHECK_EQUAL(Write(typeid(std::string *)), "std::string*");
+	BOOST_CHECK_EQUAL(Write(typeid(std::wstring)), "std::wstring");
+	BOOST_CHECK_EQUAL(Write(typeid(std::ostream)), "std::ostream");
+
+	// TODO: get rid of default template parameters, may require some sort of recursive parser
+	BOOST_CHECK_EQUAL(Write(typeid(std::map<std::string, std::string>)), "std::map<std::string, std::string>");
 }
